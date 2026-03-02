f8c6efd272483518739a31ae9940ae6e04ae7636
Egor Zhulanov <egorzhulanovbar0313@gmail.com>
2026-03-02 14:00:29 +0500

    Modules are added and information about the room residents is displayed

--- 01_inhabitants.py
+++ 01_inhabitants.py
@@ -4,3 +4,8 @@
 # Формат: В комнате room_1 живут: ...
 
 # TODO здесь ваш код
+
+import room_1, room_2
+
+print(f"В комнате room_1 живут: {', '.join(room_1.folks)}")
+print(f"В комнате room_2 живут: {', '.join(room_2.folks)}")
