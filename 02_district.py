814940ab8f12d9cfa64a13d2bf9fa5579b74208d
Egor Zhulanov <egorzhulanovbar0313@gmail.com>
2026-03-02 16:59:51 +0500

    All the people living in the district from district.

--- 02_district.py
+++ 02_district.py
@@ -7,6 +7,17 @@
 
 # TODO здесь ваш код
 
+from district.central_street.house1 import room1 as cs_h1_r1, room2 as cs_h1_r2
+from district.central_street.house2 import room1 as cs_h2_r1, room2 as cs_h2_r2
+from district.soviet_street.house1 import room1 as ss_h1_r1, room2 as ss_h1_r2
+from district.soviet_street.house2 import room1 as ss_h2_r1, room2 as ss_h2_r2
 
+rooms = [cs_h1_r1, cs_h1_r2, cs_h2_r1, cs_h2_r2, ss_h1_r1, ss_h1_r2, ss_h2_r1, ss_h2_r2]
+
+all_folks = []
+for room in rooms:
+    all_folks += room.folks
+    
+print(f"На районе живут {', '.join(all_folks)}")
 
 
